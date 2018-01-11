65f31697ed2b0583e6d5a105433eb01eb913c751
Rukeith <fenglan.crazydance@gmail.com>
2018-01-12 00:39:54 +0800

    Remove date and float point

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-$("#cal").click(function() {
-  const from = $('#from-date').val();
-  const fromTime = $('#from-time').val();
-  const to = $('#to-date').val();
-  const toTime = $('#to-time').val();
-  const fromMoment = moment(`${from} ${fromTime}`)
-  const toMoment = moment(`${to} ${toTime}`)
-  const kk = toMoment.diff(fromMoment, 'hours')
-  $('#result').text(kk);
+$(document).ready(function () {
+
+  $("#cal").click(function() {
+    const fromTime = $('#from-time').val();
+    const toTime = $('#to-time').val();
+    const fromMoment = moment(`2017-12-31 ${fromTime}`)
+    const toMoment = moment(`2017-12-31 ${toTime}`)
+    const result = toMoment.diff(fromMoment, 'hours', true)
+    $('#result').text(result);
+  });
 });
