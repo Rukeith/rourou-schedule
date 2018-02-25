01c06cbe7c630048d7325bee986fd73b355e2c90
Rukeith <fenglan.crazydance@gmail.com>
2018-02-25 20:15:30 +0800

    Update use route

--- client/index.js
+++ client/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, browserHistory, Route, IndexRoute } from "react-router";
+import { createBrowserHistory } from 'history';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from "../common/components/App";
-import HomePage from "../common/components/HomePage";
+import App from '../common/containers/App';
+// import App from "../common/components/App";
+// import HomePage from "../common/components/HomePage";
 import configureStore from '../common/store/configureStore';
 import '../common/assets/index.css';
 
 const preloadedState = window.PRELOADED_STATE;
 const store = configureStore(preloadedState);
+const history = createBrowserHistory();
 
 render(
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
+    <BrowserRouter history={history}>
+      <Route exact path="/" component={App} />
+      {/* <Route path="/" component={App}>
         <IndexRoute component={HomePage} />
-      </Route>
-    </Router>
+      </Route> */}
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
